583697d97a1a37a5d01a31aac13b86b88007cf84
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-06-08 20:00:23 -0700

    MAIN.pas
    The main script for the WacOS BaSYS calculator program.

---
+++ CALC/MAIN.pas
@@ -0,0 +1,26 @@
+-- Start of script
+-- The Calculator program for WacOS BaSYS and classic WacOS
+-- This program is incomplete, and uses pseudocode. It is not yet functional
+program calculator(output);
+begin
+  Write.title('BaSYS Calculator')
+  type buttonNum('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') in range('GUI coordinates here')
+  type buttonAdd('+') as -- Incomplete
+  type buttonSubtract('-') as -- Incomplete
+  type buttonMultiply('*') as -- Incomplete
+  type buttonDivide('/') as -- Incomplete
+  type buttonClear('C') as erase(input('all'));
+  type buttonErase('E') as erase(input(-1+));
+  return buttonNum(10)
+  return buttonAdd(1)
+  return buttonSubtract(1)
+  return buttonMultiply(1)
+  return buttonDivide(1)
+  return buttonClear(1)
+  return buttonErase(1)
+end.
+-- File info
+-- File type: Pascal source file (*.pas)
+-- File version: 1 (2022, Wednesday, June 8th at 8:00 pm)
+-- Line count (including blank lines and compiler line): 27
+-- End of script
